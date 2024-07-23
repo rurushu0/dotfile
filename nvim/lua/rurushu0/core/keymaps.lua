9ab0c1b00af28157b0e4bb12be1bdc4bbfddd7ed
zengshu <rurushu0.zen@gmail.com>
2024-07-23 15:25:48 +0900

    add nvim text editing keymaps, lsp config

--- nvim/lua/rurushu0/core/keymaps.lua
+++ nvim/lua/rurushu0/core/keymaps.lua
@@ -20,3 +20,12 @@ Map("n", "<leader>bd", ":bd<CR>") -- from Doom Emacs
 -- Map('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 -- Map('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 -- Map('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+--
+
+-- Text Editing
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+Map("v", "<", "<gv")
+Map("v", ">", ">gv")
+
--- nvim/lua/rurushu0/plugins/lsp/lsp-config.lua
+++ nvim/lua/rurushu0/plugins/lsp/lsp-config.lua
@@ -4,5 +4,52 @@ return {
     { 'williamboman/mason.nvim', config = true },
     { 'williamboman/mason-lspconfig.nvim', config = true },
     { "WhoIsSethDaniel/mason-tool-installer.nvim", config = true },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'hrsh7th/nvim-cmp' },
   },
+  config = function()
+    vim.api.nvim_create_autocmd('LspAttach', {
+      group = vim.api.nvim_create_augroup("lsp-config", {clear = true}),
+
+      callback = function()
+        local map = function(keys, func, desc)
+          vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+        end
+
+        local builtin = require('telescope.builtin')
+        map("gd", builtin.lsp_definitions, '[G]o to [D]efinitions')
+        map("gr", builtin.lsp_references, '[G]o to [R]erencens')
+      end,
+    })
+
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+
+    local servers = {}
+
+    ---
+    -- mason
+    --- 
+    require('mason').setup()
+
+    local ensure_installed = vim.tbl_keys(servers or {})
+    vim.list_extend(ensure_installed, {
+      -- Formatting
+      "stylua", -- Used to format Lua code
+    })
+    require("mason-tool-installer").setup {
+      ensure_installed = ensure_installed,
+    }
+
+    require("mason-lspconfig").setup {
+      handlers = {
+        function(server_name)
+          local server = servers[server_name] or {}
+          server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+          require('lspconfig')[server_name].setup(server)
+        end,
+      }
+    }
+
+  end,
 }
