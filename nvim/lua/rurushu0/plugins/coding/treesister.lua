241dc85d755b7b630f66e548af55c746c75c92b8
zengshu <rurushu0.zen@gmail.com>
2024-07-28 22:56:30 +0900

    refind treesister(add ensured languages)

---
+++ nvim/lua/rurushu0/plugins/coding/treesister.lua
@@ -0,0 +1,63 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPre", "BufNewFile" },
+  build = ":TSUpdate",
+  dependencies = {
+    "windwp/nvim-ts-autotag",
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  },
+
+  -- opts = {
+  --   highlight = { enable = true },
+  --   indent = { enable = true },
+  --   autotag = { enable = true },
+  --   ensure_installed = {
+  --     'bash',
+  --     'lua',
+  --   },
+  -- },
+
+  config = function()
+    local treesitter = require "nvim-treesitter.configs"
+
+    treesitter.setup {
+      highlight = {
+        enable = true,
+      },
+      indent = {
+        enable = true,
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-Space>",
+          node_incremental = "<C-Space>",
+          node_decremental = "<bs>",
+          scope_incremental = false,
+        },
+      },
+      ensure_installed = {
+        "bash",
+        "c",
+        "cpp",
+        "html",
+        "css",
+        "markdown",
+        "haskell",
+        "lua",
+        "json",
+        "java",
+        "javascript",
+        "typescript",
+        "python",
+        "sql",
+        "scala",
+        "rust",
+        "regex",
+        "vue",
+        "svelte",
+        "yaml",
+      },
+    }
+  end,
+}
