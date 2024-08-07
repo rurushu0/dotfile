c813c56c84fed5e60a366aaced1283a0777424c9
zengshu <rurushu0.zen@gmail.com>
2024-08-08 06:08:28 +0900

    floater: add keymapping

--- nvim/lua/rurushu0/plugins/term/floaterm.lua
+++ nvim/lua/rurushu0/plugins/term/floaterm.lua
@@ -1,3 +1,8 @@
 return {
   "voldikss/vim-floaterm",
+  config = function()
+    vim.keymap.set("n", "<leader>ftn", ":FloatermNew<CR>")
+    vim.keymap.set("n", "<leader>ftt", ":FloatermToggle<CR>")
+    vim.keymap.set("n", "<leader>ftp", ":FloatermPrev<CR>")
+  end,
 }
